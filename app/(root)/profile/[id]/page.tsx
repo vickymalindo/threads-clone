e3d621e8e8ee08f6f436d623ba5e140fa055377c
vickymalindo <vicky.malindo@gmail.com>
2023-09-11 11:39:13 +0700

    feat: implement ProfileHeader in Page Profile

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,26 @@
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import { fetchUser } from '@/lib/actions/user.action';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+const Page = async ({ params }: { params: { id: string } }) => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(params.id);
+  if (!userInfo?.onboarded) redirect('/onboarding');
+  return (
+    <section>
+      <ProfileHeader
+        accountId={userInfo.id}
+        authUserId={user.id}
+        name={userInfo.name}
+        username={userInfo.username}
+        imgUrl={userInfo.image}
+        bio={userInfo.bio}
+      />
+    </section>
+  );
+};
+
+export default Page;
