9ee190c9698768e9c8a0caad81307a3acae87070
vickymalindo <vicky.malindo@gmail.com>
2023-09-11 21:09:03 +0700

    feat: implement fetch user post

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
+import Thread from '../models/thread.model';
 import User from '../models/user.model';
 import { connectToDB } from '../mongoose';
 
@@ -51,5 +52,33 @@ export async function fetchUser(userId: string) {
     return await User.findOne({ id: userId });
   } catch (error: any) {
     throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+}
+
+export async function fetchUserPosts(userId: string) {
+  try {
+    connectToDB();
+
+    // Find all threads authored by the user with the given userId
+    const threads = await User.findOne({ id: userId }).populate({
+      path: 'threads',
+      model: Thread,
+      populate: [
+        {
+          path: 'children',
+          model: Thread,
+          populate: {
+            path: 'author',
+            model: User,
+            select: 'name image id', // Select the "name" and "_id" fields from the "User" model
+          },
+        },
+      ],
+    });
+    console.log(threads);
+    return threads;
+  } catch (error) {
+    console.error('Error fetching user threads:', error);
+    throw error;
   }
 }
