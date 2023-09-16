3fc0944674ba358f5ce4047acc1b1ff5edfc183c
vickymalindo <vicky.malindo@gmail.com>
2023-09-16 15:10:58 +0700

    feat: fetch data from database

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,14 @@
 import AccountProfile from '@/components/forms/AccountProfile';
+import { fetchUser } from '@/lib/actions/user.action';
 import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
 const Page = async () => {
   const user = await currentUser();
+  if (!user) return null;
 
-  const userInfo = {};
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect('/');
 
   const userData = {
     id: user?.id,
